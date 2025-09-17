a3cab8215c8e30a98cf15f449fc6c95115726fe4
Alaa Mobarak <3laa.m0o0barak@gmail.com>
2025-09-17 19:36:02 +0300

    commit merge

--- Company.Project/Company.Project.MVC/Program.cs
+++ Company.Project/Company.Project.MVC/Program.cs
@@ -1,3 +1,10 @@
+using Company.Project.Application;
+using Company.Project.Domain.Models;
+using Company.Project.DTO.DTO.OTPs;
+using Company.Project.Infrastructure;
+using Company.Project.theDbcontext;
+using Microsoft.AspNetCore.Identity;
+
 namespace Company.Project.MVC
 {
     public class Program
@@ -8,6 +15,22 @@ namespace Company.Project.MVC
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            // allow CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+            // bind email 
+            builder.Services.Configure<EmailSettingsDTO>(builder.Configuration.GetSection("EmailSettings"));
+            
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<Context>().AddDefaultTokenProviders();
+
+            // call the infrastructure and application methods
+            builder.Services.Application_CS(builder.Configuration);
+            builder.Services.Infrastructure_CS(builder.Configuration);
 
             var app = builder.Build();
 
@@ -16,10 +39,14 @@ namespace Company.Project.MVC
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            
+            
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
